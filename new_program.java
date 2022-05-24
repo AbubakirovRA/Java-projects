f75140cd88c22bdc5009f1ae35b0207fc700e3cf
AbubakirovRA <AbubakirovRA@ya.ru>
2022-05-24 21:22:47 +0500

    Initial commit

---
+++ new_program.java
@@ -0,0 +1,14 @@
+/**
+ * new_program
+ */
+public class new_program {
+
+    public static void main(String[] args) {
+        String s="Hello";
+        short age = 10 ;
+        float e = 2.7f;
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(e);
+    }
+}
