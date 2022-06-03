0ba74c1d201e3f097923d299972a719b834adf20
AbubakirovRA <AbubakirovRA@ya.ru>
2022-06-03 20:37:15 +0500

    task2&task leetcode added

---
+++ Workshop 1/task2/task2.java
@@ -0,0 +1,18 @@
+/**
+ * task2
+ */
+public class task2 {
+    public static void main(String[] args) {
+        generate(1, 7, 1, 3, "");
+    }
+    
+    public static void generate(int source, int target, int c, int d, String path) {
+        if (source>target) return;
+        if (source == target){
+            System.out.println(path);
+            return; 
+        }
+        generate(source + c, target, c, d, path + "k1 ");
+        generate(source * d, target, c, d, path + "k2 ");
+    }
+}
