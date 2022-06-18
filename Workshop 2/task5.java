8b6f80e8b94715de79798fbcae10768451af63ee
AbubakirovRA <AbubakirovRA@ya.ru>
2022-06-19 03:38:40 +0500

    tasks 4-7 added

--- Workshop 2/task5.java
+++ Workshop 2/task5.java
@@ -1,3 +1,4 @@
+
 /**
  * Задано уравнение вида q + w = e, q, w, e >= 0. 
  * Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69. 
@@ -15,15 +16,14 @@ public class task5 {
 
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> signIndexes = new ArrayList<>();
-        String text = "5? + 3? = ??";//уравнение в виде строки, мы не можем менять строки
-        char [] textChar = text.replace(" ", "").toCharArray();
-        //массив символов могу менять, вместо вопросиков поставить цифры
+        String text = "5? + 3? = ??";
+        char[] textChar = text.replace(" ", "").toCharArray();
 
-        for (int i = 0; i < textChar.length; i++){
-            if ( textChar[i] == '+' || textChar[i] == '='){
+        for (int i = 0; i < textChar.length; i++) {
+            if (textChar[i] == '+' || textChar[i] == '=') {
                 textChar[i] = '-';
             }
-            if (textChar[i] == '?'){
+            if (textChar[i] == '?') {
                 signIndexes.add(i);
             }
         }
@@ -35,23 +35,19 @@ public class task5 {
     }
 
     private static void combWithRep(int[] comb, int index, int K) {
-        if (index == comb.length){
-            //готова очередная комбинация
-            //здесь поставим на место вопросов комбинацию, проверим подходит или нет
+        if (index == comb.length) {
             checkComb(comb);
             return;
         }
 
-        for (int i = 0; i < K; i++){
+        for (int i = 0; i < K; i++) {
             comb[index] = i;
             combWithRep(comb, index + 1, K);
         }
     }
 
     public static void checkComb(int[] comb) {
-        for (int i = 0; i < comb.length; i++){
-            //sign = 2, 5
-            // comb = 1, 1
+        for (int i = 0; i < comb.length; i++) {
             textGlobal[signIndexesGlobal.get(i)] = Character.forDigit(comb[i], 10);
         }
 
@@ -60,31 +56,11 @@ public class task5 {
         int a = Integer.parseInt(numbers[0]);
         int b = Integer.parseInt(numbers[1]);
         int c = Integer.parseInt(numbers[2]);
-        
-        if (a + b == c){
+
+        if (a + b == c) {
             System.out.printf("%d + %d = %d", a, b, c);
             System.out.println();
         }
     }
-    
-}
 
-/**
-* Решение:
-* Задача решается методом перебора.
-* 1. Определяем сколько разрядов в первом числе.
-* 2. Определяем сколько разрядов во втором числе.
-* 3. Определяем какой разряд(ы) не известен(ны) в первом числе.
-* 4. Определяем какой разряд(ы) не известен(ны) во втором числе.
-* 5. Подставляем на место неизвестных разрядов первого числа 1.
-* 6. Подставляем на место неизвестных разрядов второго числа 1.
-* 7. Начиная с младшего разряда увеличиваем единицы в разрядах второго числа до 0.
-* 8. На каждой итерации проверяем равенство. Если равенство верное - выдаем ответ, выходим из программы.
-* 9. Иначе увеличиваем, начиная с младшего разряда, на единицу первое число.
-* 10. Повторяем шаги 7, 8.
-* 11. Если до сих пор ответ не найден, выдаем ответ, что решения нет.
-        // String st = "2? + ?5 = 69";
-        // st = st.replaceAll("\\s+","");      //удаляем все пробелы и невидимые символы 
-        //                                                        // (например, tab, \n)
-        // System.out.println(st);
-*/
+}
