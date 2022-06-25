924887642ff018249b974a05e6dc09196bb782c4
AbubakirovRA <AbubakirovRA@ya.ru>
2022-06-25 23:44:18 +0500

    add leetcode UndergroundSystem task

--- Workshop 4/hierarchy.java
+++ Workshop 4/hierarchy.java
@@ -1,58 +1,58 @@
-/**
- * hierarchy
- */
-class Client {
+// /**
+//  * hierarchy
+//  */
+// class Client {
 
-    private String name;
-    private String age;
-    private String passport;
-    private String phone;
+//     private String name;
+//     private String age;
+//     private String passport;
+//     private String phone;
     
-    public String getName() {
-        return name;
-    }
-    public String getAge() {
-        return age;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public String getPhone() {
-        return phone;
-    }
+//     public String getName() {
+//         return name;
+//     }
+//     public String getAge() {
+//         return age;
+//     }
+//     public void setPhone(String phone) {
+//         this.phone = phone;
+//     }
+//     public String getPhone() {
+//         return phone;
+//     }
 
-    public String getPassport() {
-        return passport;
-    }
-}
+//     public String getPassport() {
+//         return passport;
+//     }
+// }
 
-class Worker {
-    public enum Status{
-        manager, consult;
-    }
-    Status status;
-}
+// class Worker {
+//     public enum Status{
+//         manager, consult;
+//     }
+//     Status status;
+// }
 
 
-class Main {
+// class Main {
 
-    static void getInfo(Client client, Worker worker){
-        System.out.printf("name: %s\n", client.getName());
-        System.out.printf("Age: %s\n", client.getAge());
-        System.out.printf("Phone: %s\n", client.getPhone());
-        if (worker.status == Worker.Status.manager){
-            System.out.printf("Passport: '***'");
-        }else{
-            System.out.printf("Passport: %s", client.getPassport());
-        }
-    }
+//     static void getInfo(Client client, Worker worker){
+//         System.out.printf("name: %s\n", client.getName());
+//         System.out.printf("Age: %s\n", client.getAge());
+//         System.out.printf("Phone: %s\n", client.getPhone());
+//         if (worker.status == Worker.Status.manager){
+//             System.out.printf("Passport: '***'");
+//         }else{
+//             System.out.printf("Passport: %s", client.getPassport());
+//         }
+//     }
 
-    static String updateClientPhone(String str, Client client, Worker worker){
-        if (worker.status == Worker.Status.consult && !str.isBlank()){
-            client.setPhone(str);
-        }
-        return client.getPhone();
-    }
+//     static String updateClientPhone(String str, Client client, Worker worker){
+//         if (worker.status == Worker.Status.consult && !str.isBlank()){
+//             client.setPhone(str);
+//         }
+//         return client.getPhone();
+//     }
 
-}
+// }
 
