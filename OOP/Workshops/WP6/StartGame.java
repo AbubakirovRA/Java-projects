f71c793ec2006b31f957d5709cdeff2a9e3eec12
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2022-08-08 11:45:30 +0500

    game guest the number added

--- OOP/Workshops/WP6/StartGame.java
+++ OOP/Workshops/WP6/StartGame.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 
 public class StartGame {
     public static void main(String[] args) throws ClassNotFoundException, IOException {
-        Engine<Game, TerminalPrint> Round1 = new Engine<Game,TerminalPrint>(new Game(), new TerminalPrint());
-        Round1.startGame();
-
-        Engine<Game, TerminalPrint> Round2 = new Engine<Game,TerminalPrint>(new Game(), new TerminalPrint());
-        Round2.startGame();
+        Engine<Game, TerminalPrint> Begin = new Engine<Game,TerminalPrint>(new Game(), new TerminalPrint());
+        Begin.startGame();
     }
 }
