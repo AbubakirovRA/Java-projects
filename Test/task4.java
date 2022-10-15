f59c43388b7aae6bc2860d249c42ee682e4f7857
AbubakirovRA <AbubakirovRA@ya.ru>
2022-10-16 01:27:05 +0500

    added solution base data test task

--- Test/task4.java
+++ Test/task4.java
@@ -7,6 +7,7 @@
 public class task4 {
     public static void main(String[] args){
         int num = getInt.request();
+        if (num < 0) num = num * -1;
         System.out.println("Numbers of digits - " + foo.numDigits(num));
     }
 }
